b754f1ee8a541ac2ee570509779c9bee01ec2417
shijun <wnbuhao@live.com>
2018-11-24 13:11:08 +0800

    写了首首页等

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,15 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get("/user.html",function(req,res,next){
+  res.render("user");
+});
+
+router.get("/brand.html",function(req,res,next){
+  res.render("brand");
+});
+
+router.get("/phone.html",function(req,res,next){
+  res.render("phone");
+});
 module.exports = router;
